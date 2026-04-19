a76eb703bf58fa918c69605e15ef7bd99baf588f
Stephen <stephen.jszs.gplay@gmail.com>
2026-04-20 01:50:42 +0800

    chore: update sample-config

--- sample-config.js
+++ sample-config.js
@@ -5,5 +5,5 @@
 const ENV = {
   STUDENT_ID: "12345678", // Replace this with your student id no., leave it as "" otherwise
   PASSWORD: "password", // Replace this with your password, leave it as "" otherwise
-  AUTO_LOGIN: true, // Set this to true to auto login, false if otherwise
+  AUTO_LOGIN: false, // Set this to true to auto login after fill, false if otherwise
 };
